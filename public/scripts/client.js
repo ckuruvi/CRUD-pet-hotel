320c38e7cd9fcda0503346aab3a570fffd7971c7
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-01-27 15:34:50 -0600

    Completed base mode. Currently displaying multiple pets with different visit ID.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,7 @@ $(function() {
   $('#addPet').on('click',addPet);
   $('#pet-list').on('click','.update',editPetInfo);
   $('#pet-list').on('click','.delete',deletePetInfo);
-
+  $('#pet-list').on('click', '.checkInOut', toggleStatus);
 }); // end doc ready function
 
 function deletePetInfo(event){
@@ -93,6 +93,7 @@ function getPetDetails(){
 function displayPetDetails(pets){
   console.log('inside displayPetDetails function',pets);
   $('#pet-list').empty();
+
   pets.forEach(function(obj){
     var $li = $('<li></li>');
 
@@ -104,7 +105,11 @@ function displayPetDetails(pets){
   $form.append('<input type="text" name="color" value="'+obj.color+'"/>');
   $form.append('<button id="'+obj.pid+'" class="update">Update</button>');
   $form.append('<button id="'+obj.pid+'" class="delete">Delete</button>');
-  $form.append('<button class="checkInOut">In</button>');
+  if(obj.checking_flag != 1){
+  $form.append('<button data-visit="'+obj.vid+'" id="'+obj.pid+'" class="checkInOut">In</button>');
+}else{
+  $form.append('<button data-visit="'+obj.vid+'" id="'+obj.pid+'" class="checkInOut">Out</button>');
+}
    $li.append($form);
   $('#pet-list').append($li);
 });
@@ -127,4 +132,40 @@ function displayOwnerList(list){
     var $owner=('<option value="'+obj.id+'">'+obj.first_name+' '+obj.last_name+'</option>');
     $('#ownerSelector').append($owner);
   });
+}
+
+function toggleStatus(event){
+  event.preventDefault();
+  var formData = $(this).closest('form').serialize();
+  var petId=$(this).attr('id');
+  var $thisButton = $(this);
+  var checkingStatus = $thisButton.data('visit');
+  console.log(petId);
+  // console.log($(this).text());
+  if($(this).text()== 'In'){
+    $.ajax({
+      url: '/visits/'+ petId,
+      type: 'POST',
+      data: formData,
+      success: function(formData){
+        console.log('This is this:', formData[0].id);
+        getOwnerList();
+        $thisButton.data('visit', formData[0].id);
+      }
+    }); //end ajax call
+    $(this).text('Out');
+  }else{
+    var visitId = $(this).data('visit');
+    console.log('This is the visit id', visitId);
+    $.ajax({
+      url: '/visits/'+ visitId,
+      type: 'PUT',
+      data: formData,
+      success: function(formData){
+        console.log(formData);
+        getOwnerList();
+      }
+    }); //end ajax call
+    $(this).text('In');
+  }
 }
--- routes/visits.js
+++ routes/visits.js
@@ -6,18 +6,17 @@ var router = express.Router();
 var pool = new pg.Pool(config);
 
 
-router.post('/',function(req,res){
-  console.log('req.body::',req.body);
+router.get('/list',function(req,res){
   pool.connect(function(err,client,done){
     if(err){
       console.log('error connecting to DB',err);
       res.sendStatus(500);
       done();
     } else {
-
      client.query(
-      'insert into pets(owner_id,name,color,breed, display_flag) values($1,$2,$3,$4, $5) returning *;',
-      [req.body.ownerName, req.body.petName,req.body.color,req.body.breed, 1],
+       'select first_name,last_name,name,check_in,check_out from owners o join pets p on o.id=p.owner_id join visits v on p.id=v.pet_id;'
+      //  'select * from visits;'
+      ,
       function(err,result){
         done();
         if(err){
@@ -30,10 +29,11 @@ router.post('/',function(req,res){
       });
     }
   });
-});//end of post
+});//end of get
 
-router.put('/:pid',function(req,res){
+router.post('/:pid',function(req,res){
   console.log('req.body::',req.body);
+  var date = new Date().toISOString().slice(0,10);
   pool.connect(function(err,client,done){
     if(err){
       console.log('error connecting to DB',err);
@@ -42,8 +42,36 @@ router.put('/:pid',function(req,res){
     } else {
 
      client.query(
-       'UPDATE pets SET name=$2, breed=$3, color=$4 WHERE id = $1 RETURNING *',
-                    [req.params.pid, req.body.petName, req.body.breed, req.body.color],
+      'insert into visits(check_in, pet_id, checking_flag) values($1,$2,$3) returning *;',
+      [date, req.params.pid, 1],
+      function(err,result){
+        done();
+        if(err){
+          console.log('error querying db',err);
+          res.sendStatus(500);
+        } else {
+          console.log('posted info from db from visits',result.rows);
+          res.send(result.rows);
+        }
+      });
+    }
+  });
+});//end of post
+
+router.put('/:pid',function(req,res){
+  console.log('req.body::',req.body);
+  var date = new Date().toISOString().slice(0, 10);
+  console.log('This is the id we are currently looking for', req.params.pid);
+  pool.connect(function(err,client,done){
+    if(err){
+      console.log('error connecting to DB',err);
+      res.sendStatus(500);
+      done();
+    } else {
+
+     client.query(
+       'UPDATE visits SET check_out=$2, checking_flag=$3 WHERE id = $1 RETURNING *',
+                    [req.params.pid, date, 0],
       function(err,result){
         done();
         if(err){
@@ -57,31 +85,5 @@ router.put('/:pid',function(req,res){
     }
   });
 });//end of put
-
-router.delete('/:pid',function(req,res){
-  console.log('req.body::',req.body);
-  pool.connect(function(err,client,done){
-    if(err){
-      console.log('error connecting to DB',err);
-      res.sendStatus(500);
-      done();
-    } else {
-
-     client.query(
-       'UPDATE pets SET display_flag = 0 WHERE id = $1',
-                    [req.params.pid],
-      function(err,result){
-        done();
-        if(err){
-          console.log('error querying db',err);
-          res.sendStatus(500);
-        } else {
-          console.log('posted info from db',result.rows);
-          res.send(result.rows);
-        }
-      });
-    }
-  });
-});//end of post
 
 module.exports = router;
